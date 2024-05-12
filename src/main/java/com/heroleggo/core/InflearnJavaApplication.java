43441254c9ec9186542b7133789ab49d6d0d4e23
heroleggo <h3r0.y0u@gmail.com>
2024-05-12 19:43:59 +0900

    init

---
+++ src/main/java/com/heroleggo/core/InflearnJavaApplication.java
@@ -0,0 +1,13 @@
+package com.heroleggo.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heroleggo/core/InflearnJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heroleggo.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
