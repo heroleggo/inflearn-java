ce0863bb510675a05bcf307e7f3e75446a52f909
heroleggo <h3r0.y0u@gmail.com>
2024-05-16 17:51:32 +0900

    refactor: change Appconfig structure

--- src/main/java/com/heroleggo/core/config/AppConfig.java
+++ src/main/java/com/heroleggo/core/config/AppConfig.java
@@ -1,18 +1,28 @@
 package com.heroleggo.core.config;
 
+import com.heroleggo.core.order.service.DiscountService;
 import com.heroleggo.core.order.service.OrderService;
 import com.heroleggo.core.order.service.OrderServiceImpl;
 import com.heroleggo.core.order.service.RateDiscountService;
 import com.heroleggo.core.user.repository.MemoryUserRepository;
+import com.heroleggo.core.user.repository.UserRepository;
 import com.heroleggo.core.user.service.UserService;
 import com.heroleggo.core.user.service.UserServiceImpl;
 
 public class AppConfig {
     public UserService userService() {
-        return new UserServiceImpl(new MemoryUserRepository());
+        return new UserServiceImpl(userRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(userService(), new RateDiscountService());
+        return new OrderServiceImpl(userService(), discountService());
+    }
+
+    public UserRepository userRepository() {
+        return new MemoryUserRepository();
+    }
+
+    public DiscountService discountService() {
+        return new RateDiscountService();
     }
 }
