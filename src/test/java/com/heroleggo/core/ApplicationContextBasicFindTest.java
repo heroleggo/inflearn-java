a8baa0c621f7a295b1d7bf059377f5efcbcaa7ee
heroleggo <h3r0.y0u@gmail.com>
2024-06-09 20:51:51 +0900

    test: add test code on bean find methods

---
+++ src/test/java/com/heroleggo/core/ApplicationContextBasicFindTest.java
@@ -0,0 +1,45 @@
+package com.heroleggo.core;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.heroleggo.core.config.AppConfig;
+import com.heroleggo.core.user.service.UserService;
+import com.heroleggo.core.user.service.UserServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextBasicFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        UserService userService = ac.getBean("userService", UserService.class);
+        Assertions.assertThat(userService).isInstanceOf(UserServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 타입으로 조회")
+    void findBeanByType() {
+        UserService userService = ac.getBean(UserService.class);
+        Assertions.assertThat(userService).isInstanceOf(UserServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회") // 구체 타입 -> 유연성 감소
+    void findBeanByName2() {
+        UserService userService = ac.getBean("userService", UserServiceImpl.class);
+        Assertions.assertThat(userService).isInstanceOf(UserServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 불가")
+    void findBeanByNameX() {
+//        UserService notFound = ac.getBean("xxxxx", UserService.class);
+        assertThrows(NoSuchBeanDefinitionException.class,
+            () -> ac.getBean("xxxxx", UserService.class));
+    }
+}
---
+++ src/test/java/com/heroleggo/core/ApplicationContextSameBeanFindTest.java
@@ -0,0 +1,57 @@
+package com.heroleggo.core;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.heroleggo.core.config.AppConfig;
+import com.heroleggo.core.user.repository.MemoryUserRepository;
+import com.heroleggo.core.user.repository.UserRepository;
+import java.util.Map;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class ApplicationContextSameBeanFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
+
+    @Test
+    @DisplayName("타입으로 조회 시 둘 이상의 빈이 있으면, 중복 오류가 발생한다.")
+    void findBeanByTypeDuplicate() {
+        assertThrows(NoUniqueBeanDefinitionException.class, () -> ac.getBean(UserRepository.class));
+    }
+
+    @Test
+    @DisplayName("타입으로 조회 시 둘 이상의 빈이 있으면, 빈 이름을 지정")
+    void findBeanByName() {
+        UserRepository userRepository = ac.getBean("userRepository1", UserRepository.class);
+        assertThat(userRepository).isInstanceOf(UserRepository.class);
+    }
+
+    @Test
+    @DisplayName("타입으로 모두 조회")
+    void findAllBeanByType() {
+        Map<String, UserRepository> beansOfType = ac.getBeansOfType(UserRepository.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + "value = " + beansOfType.get(key));
+        }
+        System.out.println("beansOfType = " + beansOfType);
+        assertThat(beansOfType.size()).isEqualTo(2);
+    }
+
+    @Configuration
+    static class SameBeanConfig {
+        @Bean
+        public UserRepository userRepository1() {
+            return new MemoryUserRepository();
+        }
+
+        @Bean
+        public UserRepository userRepository2() {
+            return new MemoryUserRepository();
+        }
+    }
+}
